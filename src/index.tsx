44609d3cc84e674e0f3f2972481cc8f87def7e01
Alex <phoenix.trade2307@gmail.com>
2023-05-13 22:54:07 +0300

    App-4

--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,14 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { createRoot } from 'react-dom/client';
+import App_4 from "./App_4";
 
 const container  = document.getElementById('root') as HTMLElement
 const root = createRoot(container);
-root.render(<App />);
+root.render(
+    <App_4/>
+    // <App />
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
